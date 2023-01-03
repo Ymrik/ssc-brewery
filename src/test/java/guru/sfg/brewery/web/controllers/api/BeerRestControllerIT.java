216afb86f87c87241d7e603baea05883581ba05c
Umar Bariev <ubariev@mail.ru>
2023-01-03 20:52:58 +0700

    my changes

---
+++ src/test/java/guru/sfg/brewery/web/controllers/api/BeerRestControllerIT.java
@@ -0,0 +1,24 @@
+package guru.sfg.brewery.web.controllers.api;
+
+import guru.sfg.brewery.web.controllers.BaseIT;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest
+public class BeerRestControllerIT extends BaseIT {
+
+    @Test
+    void findBeers() throws Exception {
+        mockMvc.perform(get("/api/v1/beer/"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void findBeerById() throws Exception {
+        mockMvc.perform(get("/api/v1/beer/dc0891cc-0633-483c-82b1-8025a8f519e9"))
+                .andExpect(status().isOk());
+    }
+}
