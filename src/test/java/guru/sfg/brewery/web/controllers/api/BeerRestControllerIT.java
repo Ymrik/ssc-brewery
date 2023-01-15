a355692b3494c11b9d46a43cfc32a2163e0cfab1
Umar Bariev <ubariev@mail.ru>
2023-01-15 16:39:09 +0700

    my changes

--- src/test/java/guru/sfg/brewery/web/controllers/api/BeerRestControllerIT.java
+++ src/test/java/guru/sfg/brewery/web/controllers/api/BeerRestControllerIT.java
@@ -4,11 +4,19 @@ import guru.sfg.brewery.web.controllers.BaseIT;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest
 public class BeerRestControllerIT extends BaseIT {
+
+    @Test
+    public void deleteBeer() throws Exception {
+        mockMvc.perform(delete("/api/v1/beer/dc0891cc-0633-483c-82b1-8025a8f519e9")
+                        .header("Api-Key", "user").header("Api-Secret", "123"))
+                .andExpect(status().isOk());
+    }
 
     @Test
     void findBeers() throws Exception {
