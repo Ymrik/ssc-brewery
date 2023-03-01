7d003501805232344027d608d4e89e2c085e92c8
Umar Bariev <ubariev@mail.ru>
2023-03-01 10:40:42 +0700

    my changes

---
+++ src/test/java/guru/sfg/brewery/web/controllers/BreweryControllerTest.java
@@ -0,0 +1,42 @@
+package guru.sfg.brewery.web.controllers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+public class BreweryControllerTest extends BaseIT{
+    @Test
+    public void getBreweriesWithCustomerRoleTest() throws Exception {
+        mockMvc.perform(get("/brewery/api/v1/breweries")
+                .with(httpBasic("scott", "tiger")))
+                .andExpect(status().is2xxSuccessful());
+
+        mockMvc.perform(get("/brewery/breweries")
+                        .with(httpBasic("scott", "tiger")))
+                .andExpect(status().is2xxSuccessful());
+    }
+
+    @Test
+    public void getBreweriesWithNoCustomerRoleTest() throws Exception {
+        mockMvc.perform(get("/brewery/api/v1/breweries")
+                        .with(httpBasic("user", "123")))
+                .andExpect(status().isForbidden());
+
+        mockMvc.perform(get("/brewery/breweries")
+                        .with(httpBasic("user", "123")))
+                .andExpect(status().isForbidden());
+    }
+
+    @Test
+    public void getBreweriesWithoutAuth() throws Exception {
+        mockMvc.perform(get("/brewery/api/v1/breweries"))
+                .andExpect(status().isUnauthorized());
+
+        mockMvc.perform(get("/brewery/breweries"))
+                .andExpect(status().isUnauthorized());
+    }
+}
